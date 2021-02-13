f38ed9142bc7e227479ddbe7884d3f508af2bd17
BrianC <seahorse8789@gmail.com>
2021-02-13 10:17:49 -0800

    added help repromt

--- lambda/intents/factsIntent.js
+++ lambda/intents/factsIntent.js
@@ -135,7 +135,7 @@ module.exports = {
                 .speak(speakOutput)
                 // Uncomment the next line if you want to keep the session open so you can
                 // ask for another fact without first re-opening the skill
-                .shouldSessionEnd(false)
+                .withShouldEndSession(false)
                 .reprompt(data.HELP_REPROMPT)
                 .getResponse();
         },
