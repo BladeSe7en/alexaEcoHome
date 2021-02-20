e012f854382ab4fa7bd69f62bc1e73bbeb35b373
BrianC <seahorse8789@gmail.com>
2021-02-20 14:10:06 -0800

    testing what I am logging3

--- lambda/intents/reminderIntent.js
+++ lambda/intents/reminderIntent.js
@@ -92,11 +92,11 @@ module.exports = {
                 let scheduledDate = moment(reminderDate)
                 console.log('scheduledDate: ', scheduledDate)
                 //let yesterdayUTC = moment(today).subtract(1, 'days').format()
-               // let yesterday = moment(yesterdayUTC).tz("America/Los_Angeles").format();
+               // let = = moment(yesterdayUTC).tz("America/Los_Angeles").format();
                 console.log('this is today: ', today)
 
                 // Convert a time in hh:mm format to minutes
-                function timeToMins(time) {
+                const timeToMins = (time) => {
                     var b = time.split(':');
                     return b[0] * 60 + +b[1];
                 }
@@ -105,7 +105,6 @@ module.exports = {
                 let scheduledDateTime = moment(scheduledDate).add( timeToMins(time), 'minutes')
                 console.log('this is scheduledDateTime: ', scheduledDateTime.format())
                 console.log('this is time to minutes', timeToMins(time))
-
 
                 const ReminderManagementServiceClient = serviceClientFactory.getReminderManagementServiceClient();
 
@@ -128,9 +127,10 @@ module.exports = {
                         'spokenInfo': {
                             'content': [{
                                 'locale': 'en-US',
-                                'text': 'scheduledDateTime: ' + scheduledDateTime.format('YYYY-MM-DDThh:mm:ss') + ' task: ' + task + ' startDate: ',
-                                //'ssml': `<speak>${scheduledDate.format('YYYY-MM-DDThh:mm:ss') + ' ' + task}</speak>`
-                                 'ssml': '<speak> Hey look, I am speaking to you </speak>'
+                                //'text': 'scheduledDateTime: ' + scheduledDateTime.format('YYYY-MM-DDThh:mm:ss') + ' task: ' + task + ' startDate: ',
+                                'text': task,
+                                'ssml': `<speak>${task}</speak>`
+                                //'ssml': '<speak> Hey look, I am speaking to you </speak>'
                             }]
                         }
                     },
