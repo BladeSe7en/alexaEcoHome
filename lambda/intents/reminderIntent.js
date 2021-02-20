a88258be6ea747ad085f625815909d92a6204a93
BrianC <seahorse8789@gmail.com>
2021-02-20 11:11:09 -0800

    testing what I am logging3

--- lambda/intents/reminderIntent.js
+++ lambda/intents/reminderIntent.js
@@ -111,7 +111,7 @@ module.exports = {
                 }
 
                 let scheduledDateTime = moment(scheduledDate).add(timeToMins(time), 'minutes')
-                console.log('this is scheduledDateTime: ', scheduledDateTime.format())
+                console.log('this is scheduledDateTime: ', scheduledDateTime.format('YYYY-MM-DDThh:mm:ss'))
 
                 console.log('this is time to minutes', timeToMins(time))
 
