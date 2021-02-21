949d1c1f8485201c93fc355cb441fba01b177818
BrianC <seahorse8789@gmail.com>
2021-02-20 17:38:06 -0800

    testing fact reminder intent

--- lambda/intents/factReminderIntent.js
+++ lambda/intents/factReminderIntent.js
@@ -98,7 +98,7 @@ module.exports = {
                 }
 
 
-                let scheduledDateTime = moment(today).add(timeToMins(time), 'minutes')
+                let scheduledDateTime = moment(yesterday).add(timeToMins(time), 'minutes')
                 console.log('this is scheduledDateTime: ', scheduledDateTime.format())
                 console.log('this is time to minutes', timeToMins(time))
 
