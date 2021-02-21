fc3b5a0176dfca95a6b17e1efd73462bac3b7166
BrianC <seahorse8789@gmail.com>
2021-02-21 10:11:46 -0800

    added new persistance manager npm package

--- lambda/intents/factsIntent.js
+++ lambda/intents/factsIntent.js
@@ -1,5 +1,5 @@
 const Alexa = require('ask-sdk-core');
-const  ecoFacts = require('../ecoFacts');
+const  { ecoFacts } = require('../ecoFacts');
 
 module.exports = {
     GetNewFactHandler: {
@@ -11,8 +11,9 @@ module.exports = {
                     && request.intent.name === 'GetNewFactIntent');
         },
         handle(handlerInput) {
-            const { speakOutput, SKILL_NAME, GET_FACT_MESSAGE, HELP_MESSAGE, HELP_REPROMPT, FALLBACK_MESSAGE, FALLBACK_REPROMPT, ERROR_MESSAGE, STOP_MESSAGE } = ecoFacts();
+            const { speakOutput, SKILL_NAME, GET_FACT_MESSAGE, HELP_MESSAGE, HELP_REPROMPT, FALLBACK_MESSAGE, FALLBACK_REPROMPT, ERROR_MESSAGE, STOP_MESSAGE } = ecoFacts.getData();
             console.log('speakOutput: ', speakOutput)
+            
 
             return handlerInput.responseBuilder
                 .speak(`${speakOutput} If you would like to hear another fact, ask me again.`)
