14413825e7369771a57cd09d57c3d264595053f0
BrianC <seahorse8789@gmail.com>
2021-02-21 01:11:55 -0800

    added new persistance manager npm package

--- lambda/intents/factReminderIntent.js
+++ lambda/intents/factReminderIntent.js
@@ -65,7 +65,6 @@ module.exports = {
     FactReminderHandler: {
         canHandle(handlerInput) {
             const { request } = handlerInput.requestEnvelope;
-            console.log('this is intent name: ', request.intent.name)
             return request.type === 'IntentRequest' && request.intent.name === 'FactReminderIntent';
 
         },
