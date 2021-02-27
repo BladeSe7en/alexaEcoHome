2aaa17a356067953e5f80a62573e06dee72d9b80
BrianC <seahorse8789@gmail.com>
2021-02-27 12:48:03 -0800

    checking if request.intent.name can be equal to two different names

--- lambda/intents/factsIntent.js
+++ lambda/intents/factsIntent.js
@@ -1,22 +1,23 @@
 const Alexa = require('ask-sdk-core');
-const  { ecoFacts } = require('../ecoFacts');
+const { ecoFacts } = require('../ecoFacts');
 
 module.exports = {
     GetNewFactHandler: {
         canHandle(handlerInput) {
             const request = handlerInput.requestEnvelope.request;
 
-            return request.type === 'LaunchRequest'
+            return (request.type === 'LaunchRequest'
                 || (request.type === 'IntentRequest'
-                    && request.intent.name === 'GetNewFactIntent') ||
-                    request.type === 'LaunchRequest'
-                || (request.type === 'IntentRequest'
-                    && request.intent.name === 'GetNewFactIntent');
+                    && request.intent.name === 'GetNewFactIntent'))
+                ||
+                (request.type === 'LaunchRequest'
+                    || (request.type === 'IntentRequest'
+                        && request.intent.name === 'AMAZON.YesIntent'));
         },
         handle(handlerInput) {
             const { speakOutput, help } = ecoFacts.getData();
             console.log('speakOutput: ', speakOutput)
-            
+
 
             return handlerInput.responseBuilder
                 .speak(`${speakOutput} If you would like to hear another fact, ask me again.`)
