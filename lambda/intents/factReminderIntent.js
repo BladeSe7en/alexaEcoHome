cf3c68ec793bc7140f6c88c906e3060fe64b6736
BrianC <seahorse8789@gmail.com>
2021-03-05 16:17:02 -0800

    testing sams freq suggestion for daily

--- lambda/intents/factReminderIntent.js
+++ lambda/intents/factReminderIntent.js
@@ -377,12 +377,12 @@ module.exports = {
                         "scheduledTime": scheduledDateTime.format('YYYY-MM-DDTHH:mm:ss'),
                         "timeZoneId": "America/Los_Angeles",
                         freq: 'DAILY',
-                        "recurrence": {
-                            "startDateTime": today,
-                            // "recurrenceRules": [
-                            //     `FREQ=${freq};BYHOUR=${minutes[0]};BYMINUTE=${minutes[1]};BYSECOND=0;INTERVAL=1;`,
-                            // ]
-                        }
+                        // "recurrence": {
+                        //     "startDateTime": today,
+                        //     // "recurrenceRules": [
+                        //     //     `FREQ=${freq};BYHOUR=${minutes[0]};BYMINUTE=${minutes[1]};BYSECOND=0;INTERVAL=1;`,
+                        //     // ]
+                        // }
                     },
                     'alertInfo': {
                         'spokenInfo': {
